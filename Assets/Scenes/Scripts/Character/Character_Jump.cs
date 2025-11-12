1a7b75f326e85faeb396ff6b8d1c847f35ad9b59
ghost-backseat-driver <ysl2323@naver.com>
2025-11-12 18:32:59 +0900

    <Level1> 버그 관련 아.. 플레이어 이동방향이랑 카메라 이동방향을 일치시켜 버리니까,, 등을 못돌리는구나! 와! 마우스로 시점 변경하게 만들거나, 아니면, 이 컨셉 그대로 가져가서, 아예 S 키 후진 인풋을 없애버리는것도 나쁘지않아. 그럼 마우스 구현안해도 되고, 완성도는 떨어지겠지만,,, 일단 고려해보고, 추후에 고치자 일단 진짜 점프부터 -수정고려- 1.카메라 마우스 회전으로 시점변경 가능하게 할것인지 2.플레이어 컨트롤 s 인풋을 아예 제거해버릴지

--- Assets/Scenes/Scripts/Character/Character_Jump.cs
+++ Assets/Scenes/Scripts/Character/Character_Jump.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class Character_Jump : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
